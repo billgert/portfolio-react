052415caddd74dcb02fb2f852c69fc1421115323
Patrik Billgert <patrik.billgert@bbh-stockholm.se>
2018-04-25 15:40:50 +0200

    Styling the content

--- src/components/work/element/WorkElement.js
+++ src/components/work/element/WorkElement.js
@@ -4,17 +4,10 @@ import './WorkElement.css';
 export default class WorkElement extends React.Component {
   render() {
     const project = this.props.project
-    var backgroundStyle = {
-      display: 'flex',
-      margin: '0px',
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundImage: 'linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.2)), url(' + project.cover + ')'
-    };
     return (
-      <div style={backgroundStyle}>
+      <div style={backgroundStyle(project)}>
         <div className="content">
-          <Logo company={project.company}/>
+          <Logo url={project.company.logo}/>
           <Role text={project.role}/>
           <Description text={project.description}/>
         </div>
@@ -23,9 +16,19 @@ export default class WorkElement extends React.Component {
   }
 }
 
+function backgroundStyle(project) {
+  return {
+    display: 'flex',
+    margin: '0px',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundImage: 'linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.2)), url(' + project.cover + ')'
+  }
+}
+
 function Logo(props) {
   return <div className="logo">
-    <img src={props.company.logo} alt=""/>
+    <img src={props.url} alt=""/>
   </div>
 }
 
