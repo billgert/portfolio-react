cfd9ef1b15bc8d79ea532c8d0303c58485b873cf
Patrik Billgert <patrik.billgert@bbh-stockholm.se>
2018-04-25 16:47:14 +0200

    Keep images locally and referenced in js file instead of json

---
+++ src/assets/data.js
@@ -0,0 +1,226 @@
+export const projects = [
+  {
+    id: 0,
+    role: "Head of Mobile",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat tincidunt nibh quis vestibulum. Nam commodo quam mi, sed interdum dui tincidunt nec. Vestibulum odio ligula, imperdiet sit.",
+    cover: require('./hint/cover.png'),
+    started: "",
+    ended: "",
+    company: {
+      name: "Hint AB",
+      website: "",
+      logo: require('./hint/logo.png')
+    },
+    product: {
+      name: "Hint",
+      description: "",
+      website: "",
+      logo: ""
+    },
+    platforms: [
+      {
+        name: "",
+        ide: "",
+        language: "",
+        architectures: [],
+        paradigms: []
+      }
+    ],
+    team: [
+      {
+        name: "",
+        role: ""
+      }
+    ],
+    pictures: [
+      {
+        name: "",
+        description: "",
+        url: ""
+      }
+    ]
+  },
+  {
+    id: 1,
+    role: "iOS Lead",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat tincidunt nibh quis vestibulum. Nam commodo quam mi, sed interdum dui tincidunt nec. Vestibulum odio ligula, imperdiet sit.",
+    cover: require('./maitres/cover.png'),
+    started: "",
+    ended: "",
+    company: {
+      name: "Maitres AB",
+      website: "",
+      logo: require('./maitres/logo.png')
+    },
+    product: {
+      name: "MAÎTRES",
+      description: "",
+      website: "",
+      logo: ""
+    },
+    platforms: [
+      {
+        name: "",
+        ide: "",
+        language: "",
+        architectures: [],
+        paradigms: []
+      }
+    ],
+    team: [
+      {
+        name: "",
+        role: ""
+      }
+    ],
+    pictures: [
+      {
+        name: "",
+        description: "",
+        url: ""
+      }
+    ]
+  },
+  {
+    id: 2,
+    role: "iOS Lead",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat tincidunt nibh quis vestibulum. Nam commodo quam mi, sed interdum dui tincidunt nec. Vestibulum odio ligula, imperdiet sit.",
+    cover: require('./tui/cover.png'),
+    started: "",
+    ended: "",
+    company: {
+      name: "Tuifly Nordic AB",
+      website: "",
+      logo: require('./tui/logo.png')
+    },
+    product: {
+      name: "Bamseklubben",
+      description: "",
+      website: "",
+      logo: ""
+    },
+    platforms: [
+      {
+        name: "",
+        ide: "",
+        language: "",
+        architectures: [],
+        paradigms: []
+      }
+    ],
+    team: [
+      {
+        name: "",
+        role: ""
+      }
+    ],
+    pictures: [
+      {
+        name: "",
+        description: "",
+        url: ""
+      }
+    ]
+  },
+  {
+    id: 3,
+    role: "iOS Developer",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat tincidunt nibh quis vestibulum. Nam commodo quam mi, sed interdum dui tincidunt nec. Vestibulum odio ligula, imperdiet sit.",
+    cover: require('./bokus/cover.png'),
+    started: "",
+    ended: "",
+    company: {
+      name: "Bokus AB",
+      website: "",
+      logo: require('./bokus/logo.png')
+    },
+    product: {
+      name: "Bokus Play",
+      description: "",
+      website: "",
+      logo: ""
+    },
+    platforms: [
+      {
+        name: "",
+        ide: "",
+        language: "",
+        architectures: [],
+        paradigms: []
+      }
+    ],
+    team: [
+      {
+        name: "",
+        role: ""
+      }
+    ],
+    pictures: [
+      {
+        name: "",
+        description: "",
+        url: ""
+      }
+    ]
+  },
+  {
+    id: 4,
+    role: "iOS Developer",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat tincidunt nibh quis vestibulum. Nam commodo quam mi, sed interdum dui tincidunt nec. Vestibulum odio ligula, imperdiet sit.",
+    cover: require('./rebtel/cover.png'),
+    started: "",
+    ended: "",
+    company: {
+      name: "Rebtel AB",
+      website: "",
+      logo: require('./rebtel/logo.png')
+    },
+    product: {
+      name: "Rebtel Activist",
+      description: "",
+      website: "",
+      logo: ""
+    },
+    platforms: [
+      {
+        name: "",
+        ide: "",
+        language: "",
+        architectures: [],
+        paradigms: []
+      }
+    ],
+    team: [
+      {
+        name: "",
+        role: ""
+      }
+    ],
+    pictures: [
+      {
+        name: "",
+        description: "",
+        url: ""
+      }
+    ]
+  }
+]
+
+export const about = {
+  "description": "I'm a passionate mobile developer with love for Apple. Working at Hint to make way for an emerging industry we like to call think-tech.",
+  "email": "p.billgert@gmail.com",
+  "links": [
+    {
+      "url": "",
+      "logo": ""
+    },
+    {
+      "url": "",
+      "logo": ""
+    },
+    {
+      "url": "",
+      "logo": ""
+    }
+  ]
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import WorkList from './work/list/WorkList'
-import projects from '../data/projects.json'
 import './App.css'
 
 export default class App extends React.Component {
   render() {
+    const data = require('../assets/data.js')
     return (
       <div className="background">
-        <WorkList projects={projects}/>
+        <WorkList projects={data.projects}/>
       </div>
     )
   }
