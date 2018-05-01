7a14b5282b6277c0d44939155675c5af8da31fb4
Patrik Billgert <patrik.billgert@bbh-stockholm.se>
2018-05-01 16:37:45 +0200

    Open mail app on click

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,15 +3,14 @@ import './Contact.css'
 
 export default class Contact extends React.Component {
   render() {
-    const email = this.props.email
     return (
       <div className="contactContent">
-        <button className="emailButton" onClick={this.props.handleClick}>Say hi!</button>
+        <button className="emailButton" onClick={this.handleClick}>Say hi!</button>
       </div>
     )
   }
 
   handleClick = () => {
-    // Open email
+    window.location.href = "mailto:" + this.props.email
   }
 }
