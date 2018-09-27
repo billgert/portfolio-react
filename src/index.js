fff0bd5c53c91b08e6944905cf571d6259341bd1
Patrik Billgert <p.billgert@gmail.com>
2018-09-27 11:51:29 +0200

    Initialize analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import WebFont from 'webfontloader';
+import WebFont from 'webfontloader'
 
 WebFont.load({
   google: {
