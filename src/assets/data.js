77a58b158dd1a63a483f425c9fb2fd3086c9d42f
Patrik Billgert <p.billgert@gmail.com>
2018-09-30 20:02:45 +0200

    Update data file

--- src/assets/data.js
+++ src/assets/data.js
@@ -5,7 +5,7 @@ export const projects = [
     id: 0,
     active: true,
     role: "iOS Lead",
-    description_div: <div>Working on a new service called Hint that soon will be launched in beta. As one of the first developers I'm excited to work with likeminded people to create something truly awesome.</div>,
+    description: "Working on a new service called Hint that soon will be launched in beta. As one of the first developers I'm excited to work with likeminded people to create something truly awesome.",
     cover: require('./projects/hint/cover.jpg'),
     employer: {
       name: "Hint AB",
@@ -47,11 +47,11 @@ export const projects = [
     id: 1,
     active: false,
     role: "iOS Developer",
-    description_div: <div>Bokus has entered the booming industry of audiobooks with a brand new app. Mainly responsible for implementing the design and user experience in the first version. Made for {<a href="http://bbhstockholm.se/">BBH Stockholm</a>}.</div>,
+    description: "Bokus has entered the booming industry of audiobooks with a brand new app. Mainly responsible for implementing the design and user experience in the first version.",
     cover: require('./projects/bokus/cover.jpg'),
     employer: {
       name: "BBH Stockholm",
-      website: "http://bbhstockholm.se/",
+      website: "https://www.bbhstockholm.se/bokus-play",
       logo: ""
     },
     product: {
@@ -108,11 +108,11 @@ export const projects = [
     id: 2,
     active: false,
     role: "iOS Lead",
-    description_div: <div>As I have experience working in the restaurant-business this project was extra fun. MAÎTRES lets you connect with restaurants to book, pay and rate without having to wait for the bill. Made for {<a href="http://bbhstockholm.se/">BBH Stockholm</a>}.</div>,
+    description: "As I have experience working in the restaurant-business this project was extra fun. MAÎTRES lets you connect with restaurants to book, pay and rate without having to wait for the bill.",
     cover: require('./projects/maitres/cover.jpg'),
     employer: {
       name: "BBH Stockholm",
-      website: "http://bbhstockholm.se/",
+      website: "https://www.bbhstockholm.se/maitres",
       logo: ""
     },
     product: {
@@ -175,7 +175,7 @@ export const projects = [
     id: 3,
     active: false,
     role: "Mobile Developer",
-    description_div: <div>Bamseklubben is a game starring the Swedish cartoon character Bamse. Developed cross-platform for the Nordics with over 10.000 kids playing in the first weeks. Made for {<a href="http://bbhstockholm.se/">BBH Stockholm</a>}.</div>,
+    description: "Bamseklubben is a game starring the Swedish cartoon character Bamse. Developed cross-platform for the Nordics with over 10.000 kids playing in the first weeks.",
     cover: require('./projects/tui/cover.jpg'),
     employer: {
       name: "BBH Stockholm",
@@ -239,7 +239,7 @@ export const projects = [
     id: 4,
     active: false,
     role: "iOS Lead",
-    description_div:  <div>I helped define, design and develop a prototype app for Dashl in their early stages. Their network of stylists is now the go-to-place for beauty treatments in Stockholm.</div>,
+    description: "I helped define, design and develop a prototype app for Dashl in their early stages. Their network of stylists is now the go-to-place for beauty treatments in Stockholm.",
     cover: require('./projects/dashl/cover.jpg'),
     employer: {
       name: "Dashl AB",
@@ -286,11 +286,11 @@ export const projects = [
     id: 5,
     active: false,
     role: "iOS Developer",
-    description_div: <div>The Activist-program was made to create jobs by making loyal customers employees of Rebtel. With an office in their hand the Rebtel Activist can sign up new customers directly in the app. Made for {<a href="http://bbhstockholm.se/">BBH Stockholm</a>}.</div>,
+    description: "The Activist-program was made to create jobs by making loyal customers employees of Rebtel. With an office in their hand the Rebtel Activist can sign up new customers directly in the app.",
     cover: require('./projects/rebtel/cover.jpg'),
     employer: {
       name: "BBH Stockholm",
-      website: "http://bbhstockholm.se/",
+      website: "https://www.bbhstockholm.se/rebtel",
       logo: ""
     },
     product: {
@@ -348,7 +348,7 @@ export const projects = [
     id: 6,
     active: false,
     role: "iOS Developer",
-    description_div: <div>Each person is one song and together they become a playlist. OneSong aimed to be the daily soundtrack of you and your social network. Think of it like Spotify and Instagram having a baby. Made for {<a href="http://www.frontcell.se/">Frontcell</a>}.</div>,
+    description: "Each person is one song and together they become a playlist. OneSong aimed to be the daily soundtrack of you and your social network. Think of it like Spotify and Instagram having a baby.",
     cover: require('./projects/onesong/cover.jpg'),
     employer: {
       name: "Frontcell AB",
